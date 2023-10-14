797b9f1f197fb2e315577b6920c3f78a32ff8a6e
Alexander Savitskiy <64911519+Cr1stalGG@users.noreply.github.com>
2023-10-14 19:44:03 +0300

    update project

--- carsharing-core/src/main/java/by/grodno/carsharing/CarsharingApplication.java
+++ carsharing-core/src/main/java/by/grodno/carsharing/CarsharingApplication.java
@@ -3,7 +3,7 @@ package by.grodno.carsharing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "by.grodno.carsharing")
 public class CarsharingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CarsharingApplication.class, args);
